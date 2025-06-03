d91f1e136a69297684ec4c0d42caad7e5fafdf96
keyva <keyvanmatinrad@outlook.com>
2025-06-03 14:29:59 +0330

    earased

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,14 +1,7 @@
 'use client';
 
 import React from 'react';
-import Card from '@/app/components/dashboard/Card';
 
 export default function Page() {
-  return (
-    <div className="flex h-full flex-col items-center justify-center">
-      <div className="w-1/2">
-        <Card />
-      </div>
-    </div>
-  );
+  return <div></div>;
 }
