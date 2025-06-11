f4e335db1ee0ccd1267d4b72581a6eb6b49abc21
keyva <keyvanmatinrad@outlook.com>
2025-06-11 14:43:48 +0330

    fixed layout paddings

--- app/(panel)/layout.tsx
+++ app/(panel)/layout.tsx
@@ -10,11 +10,15 @@ export default function PanelLayout({
 }) {
   return (
     <div className="flex min-h-screen bg-neutral-800">
+      <Sidebar />
       <div className="flex w-full flex-col lg:mr-64">
         <Header />
-        <main className="flex-1 p-8">{children}</main>
+        <main className="flex-1 overflow-auto bg-[#262626] [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:bg-neutral-600 [&::-webkit-scrollbar-thumb]:rounded-full">
+          <div className="min-h-full">
+            {children}
+          </div>
+        </main>
       </div>
-      <Sidebar />
     </div>
   );
 }
